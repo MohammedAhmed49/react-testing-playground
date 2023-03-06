d290060e50e03376ddade7ffc9a2a63d7fd5f910
mohammed ahmed <mohammed_ahmed4949@yahoo.com>
2023-03-06 20:40:46 +0200

    code and test scoop options images from server using mock service worker

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,7 +4,7 @@ export const handlers = [
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Mint chip", imagePath: "/images/mint-chip.png" },
+        { name: "Chocolate", imagePath: "/images/mint-chip.png" },
         {
           name: "Vanilla",
           imagePath: "/images/vanilla.png",
