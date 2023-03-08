26201e1cb020aefe47f43e490f4313eb986e55c2
mohammed ahmed <mohammed_ahmed4949@yahoo.com>
2023-03-08 02:36:41 +0200

    testing topping option and showing alerts when get error from server

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -11,3 +11,21 @@ test("displays image for each scoop option from server", async () => {
   const altTexts = scoopImages.map((img) => img.alt);
   expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("display image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+
+  expect(toppingsImages).toHaveLength(3);
+
+  const altTexts = toppingsImages.map((item) => item.alt);
+
+  expect(altTexts).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
