29e5dec4e70622a5435623b9d8cd200962200cb0
mohammed ahmed <mohammed_ahmed4949@yahoo.com>
2023-03-16 10:32:56 +0200

    finished

---
+++ src/tests/orderPhase.test.jsx
@@ -0,0 +1,78 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+test("Order phases for happy path", async () => {
+  const user = userEvent.setup();
+  // render app
+  const { unmount } = render(<App />);
+
+  // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+
+  // find and click order button
+  const orderSummaryButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  await user.click(orderSummaryButton);
+
+  //check summary info based on order
+  const summaryHeading = screen.getByRole("heading", { name: "Order Summary" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole("heading", { name: "Scoops: $6.00" });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.getByRole("heading", {
+    name: "Toppings: $1.50",
+  });
+  expect(toppingsHeading).toBeInTheDocument();
+
+  expect(screen.getByText("1 Vanilla")).toBeInTheDocument();
+  expect(screen.getByText("2 Chocolate")).toBeInTheDocument();
+  expect(screen.getByText(/Cherries/i)).toBeInTheDocument();
+  // accept t&c and click confirmation button
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  await user.click(tcCheckbox);
+
+  // confirm order number on confirmation page
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  await user.click(confirmOrderButton);
+
+  const thankYouHeader = await screen.findByRole("heading", {
+    name: /thank you/i,
+  });
+  expect(thankYouHeader).toBeInTheDocument();
+
+  const orderNumber = await screen.findByText(/order number/i);
+  expect(orderNumber).toBeInTheDocument();
+
+  // click new order button
+  const newOrderButton = screen.getByRole("button", { name: /new order/i });
+  await user.click(newOrderButton);
+
+  // check if scoops and toppings have been reset
+  const scoopsTotal = await screen.findByText("Scoops total: $0.00");
+  expect(scoopsTotal).toBeInTheDocument();
+  const toppingsTotal = screen.getByText("Toppings total: $0.00");
+  expect(toppingsTotal).toBeInTheDocument();
+
+  unmount();
+});
